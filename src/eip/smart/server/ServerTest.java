944490b5ac699eebe9e7d296695ae75f1d59b823
Vincent Buresi <vincent.buresi@gmail.com>
2014-10-02 06:59:29 -0400

    Added server test files and dependency to cs-commons jar file

---
+++ src/eip/smart/server/ServerTest.java
@@ -0,0 +1,34 @@
+package eip.smart.server;
+
+import eip.smart.cscommons.CSCommons;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by vincent buresi on 10/2/14.
+ */
+public class ServerTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        answer(writer, "GET");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        answer(writer, "POST");
+    }
+
+    protected void answer(PrintWriter writer, String requestType) {
+        writer.println("Server is up and running ! Congratulations !");
+        writer.println("version : " + CSCommons.getVersion());
+        writer.println("request type : " + requestType);
+    }
+}
