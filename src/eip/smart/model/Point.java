36aa8094298696ee7ee4d92364c1078d6696d7df
Pierre Demessence <pierredemessence@gmail.com>
2014-10-10 13:09:15 +0200

    Added a Polygon class and modified Point class. Polygon class can automatically calculate perimeter and area. Added a PointTest Servlet accessible at /point

--- src/eip/smart/model/Point.java
+++ src/eip/smart/model/Point.java
@@ -4,13 +4,30 @@ package eip.smart.model;
  * Created by Pierre Demessence on 09/10/2014.
  */
 public class Point {
-    private float x;
-    private float y;
-    private float z;
+    private double x;
+    private double y;
+    private double z;
 
-    public Point(float x, float y, float z) {
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public double getDistance(Point p)
+    {
+        return (Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2) + Math.pow(this.z - p.z, 2)));
     }
 }
