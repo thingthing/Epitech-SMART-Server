e12e074d886ee00bb0062697909ad3878eab7b88
Pierre Demessence <pierredemessence@gmail.com>
2014-12-08 22:14:20 +0100

    Updated the Servlets to use annotation system.

--- src/eip/smart/server/ModelingTest.java
+++ src/eip/smart/server/ModelingTest.java
@@ -1,67 +1,71 @@
 package eip.smart.server;
 
-import eip.smart.model.Modeling;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import eip.smart.model.Modeling;
 
 /**
  * Created by vincent buresi on 10/2/14.
  */
+@WebServlet(name = "ModelingTest", urlPatterns = "/modeling")
 public class ModelingTest extends HttpServlet {
 
-    private int delay = 1000;
+	private int	delay	= 1000;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        try {
-            this.delay = Integer.parseInt(req.getParameter("delay"));
-        } catch (Exception e) {
-            this.delay = 1000;
-        }
-        answer(writer, "GET");
-    }
+	protected void answer(PrintWriter writer, String requestType) {
+		Modeling modeling = new Modeling();
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        answer(writer, "POST");
-    }
+		try {
+			System.out.println("Start of Modeling");
+			System.out.println();
+			System.out.println("===== ===== ===== ===== =====");
+			modeling.dumpAgents();
+			System.out.println("----- ----- ----- ----- -----");
+			System.out.println("Modeling at " + modeling.getCompletion() + "%");
+			System.out.println("===== ===== ===== ===== =====");
+			System.out.println();
+			Thread.currentThread();
+			Thread.sleep(this.delay);
+			while (modeling.getCompletion() < 100.0d) {
+				System.out.println();
+				System.out.println("===== ===== ===== ===== =====");
+				modeling.run();
+				System.out.println("----- ----- ----- ----- -----");
+				modeling.dumpAgents();
+				System.out.println("----- ----- ----- ----- -----");
+				System.out.println("Modeling at " + modeling.getCompletion() + "%");
+				System.out.println("===== ===== ===== ===== =====");
+				System.out.println();
+				Thread.currentThread();
+				Thread.sleep(this.delay);
+			}
+			System.out.println("End of Modeling");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-    protected void answer(PrintWriter writer, String requestType) {
-        Modeling modeling = new Modeling();
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		try {
+			this.delay = Integer.parseInt(req.getParameter("delay"));
+		} catch (Exception e) {
+			this.delay = 1000;
+		}
+		this.answer(writer, "GET");
+	}
 
-        try {
-            System.out.println("Start of Modeling");
-            System.out.println();
-            System.out.println("===== ===== ===== ===== =====");
-            modeling.dumpAgents();
-            System.out.println("----- ----- ----- ----- -----");
-            System.out.println("Modeling at " + modeling.getCompletion() + "%");
-            System.out.println("===== ===== ===== ===== =====");
-            System.out.println();
-            Thread.currentThread().sleep(this.delay);
-            while (modeling.getCompletion() < 100.0d) {
-                System.out.println();
-                System.out.println("===== ===== ===== ===== =====");
-                modeling.run();
-                System.out.println("----- ----- ----- ----- -----");
-                modeling.dumpAgents();
-                System.out.println("----- ----- ----- ----- -----");
-                System.out.println("Modeling at " + modeling.getCompletion() + "%");
-                System.out.println("===== ===== ===== ===== =====");
-                System.out.println();
-                Thread.currentThread().sleep(this.delay);
-            }
-            System.out.println("End of Modeling");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		this.answer(writer, "POST");
+	}
 }
--- src/eip/smart/server/PointTest.java
+++ src/eip/smart/server/PointTest.java
@@ -1,39 +1,41 @@
 package eip.smart.server;
 
-import eip.smart.model.Point;
-import eip.smart.model.Polygon;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+
+import eip.smart.model.Point;
+import eip.smart.model.Polygon;
 
 /**
  * Created by vincent buresi on 10/2/14.
  */
+@WebServlet(name = "PointTest", urlPatterns = "/point")
 public class PointTest extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        answer(writer, "GET");
-    }
+	protected void answer(PrintWriter writer, String requestType) {
+		Polygon p = new Polygon();
+		p.add(new Point(0, 0, 0));
+		p.add(new Point(0, 5, 0));
+		p.add(new Point(5, 5, 0));
+		p.add(new Point(5, 0, 0));
+		writer.println("Square perimeter of size 5 : " + p.getPerimeter());
+		writer.println("Square area of size 5 : " + p.getArea());
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        answer(writer, "POST");
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		this.answer(writer, "GET");
+	}
 
-    protected void answer(PrintWriter writer, String requestType) {
-        Polygon p = new Polygon();
-        p.add(new Point(0, 0, 0));
-        p.add(new Point(0, 5, 0));
-        p.add(new Point(5, 5, 0));
-        p.add(new Point(5, 0, 0));
-        writer.println("Square perimeter of size 5 : " + p.getPerimeter());
-        writer.println("Square area of size 5 : "+p.getArea());
-    }
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		this.answer(writer, "POST");
+	}
 }
