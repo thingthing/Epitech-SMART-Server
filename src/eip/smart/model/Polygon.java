b034c7209d54ae456c3b4639b35a13df00098116
Pierre Demessence <pierredemessence@gmail.com>
2014-12-09 01:10:11 +0100

    Refactored some code.

--- src/eip/smart/model/Polygon.java
+++ src/eip/smart/model/Polygon.java
@@ -7,74 +7,75 @@ import java.util.ArrayList;
  */
 public class Polygon {
 
-    private ArrayList<Point>    points;
+	private ArrayList<Point>	points;
 
-    public Polygon() {
-        this.points = new ArrayList<Point>();
-    }
+	public Polygon() {
+		this.points = new ArrayList<>();
+	}
 
-    public Polygon(ArrayList<Point> points) {
-        this.points = points;
-    }
+	public Polygon(ArrayList<Point> points) {
+		this.points = points;
+	}
 
-    /**
-     * Check if the polygon is "finite".
-     * @return true if the Polygon contains at least 3 points.
-     */
-    public boolean isFinite()
-    {
-        return (this.points.size() > 2);
-    }
+	public boolean add(Point point) {
+		return this.points.add(point);
+	}
 
-    public double getPerimeter() {
-        double perimeter = 0;
-        Point lastPoint;
+	/**
+	 * Return the area of the polygon using only the x and y coordinates of the points.
+	 * 
+	 * @return the area of the polygon.
+	 */
+	public double getArea() {
+		double area = 0;
+		int j;
 
-        if (!this.isFinite())
-            return (0);
+		j = this.points.size() - 1;
+		for (int i = 0; i < this.points.size(); i++) {
+			area += (this.points.get(j).getX() + this.points.get(i).getX()) * (this.points.get(j).getY() - this.points.get(i).getY());
+			j = i;
+		}
+		return (area / 2.0d);
+	}
 
-        lastPoint = this.points.get(0);
-        for (int i = 1 ; i < this.points.size() ; i++) {
-            perimeter += lastPoint.getDistance(this.points.get(i));
-            lastPoint = this.points.get(i);
-        }
-        perimeter += lastPoint.getDistance(this.points.get(0));
-        return (perimeter);
-    }
+	public double getPerimeter() {
+		double perimeter = 0;
+		Point lastPoint;
 
-    /**
-     * Return the area of the polygon using only the x and y coordinates of the points.
-     * @return the area of the polygon.
-     */
-    public double getArea() {
-        double area = 0;
-        int j;
+		if (!this.isFinite())
+			return (0);
 
-        j = this.points.size() - 1;
-        for (int i = 0 ; i < this.points.size() ; i++) {
-            area += (this.points.get(j).getX() + this.points.get(i).getX()) * (this.points.get(j).getY() - this.points.get(i).getY());
-            j = i;
-        }
-        return (area / 2.0d);
-    }
+		lastPoint = this.points.get(0);
+		for (int i = 1; i < this.points.size(); i++) {
+			perimeter += lastPoint.getDistance(this.points.get(i));
+			lastPoint = this.points.get(i);
+		}
+		perimeter += lastPoint.getDistance(this.points.get(0));
+		return (perimeter);
+	}
 
-    public boolean add(Point point) {
-        return points.add(point);
-    }
+	/**
+	 * Checks if the
+	 * 
+	 * @param point
+	 *            the point to check.
+	 * @return true if the point is inside the polygon.
+	 *         http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html#The C Code
+	 */
+	public boolean includes(Point point) {
+		boolean res = false;
+		for (int i = 0, j = this.points.size() - 1; i < this.points.size(); j = i++)
+			if (((this.points.get(i).getY() > point.getY()) != (this.points.get(j).getY() > point.getY())) && (point.getX() < ((this.points.get(j).getX() - this.points.get(i).getX()) * (point.getY() - this.points.get(i).getY()) / (this.points.get(j).getY() - this.points.get(i).getY()) + this.points.get(i).getX())))
+				res = !res;
+		return (res);
+	}
 
-    /**
-     * Checks if the
-     * @param point the point to check.
-     * @return true if the point is inside the polygon.
-     * http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html#The C Code
-     */
-    public boolean includes(Point point) {
-        boolean res = false;
-        for (int i = 0, j = this.points.size() - 1 ; i < this.points.size() ; j = i++) {
-            if ( ( (this.points.get(i).getY() > point.getY()) != (this.points.get(j).getY() > point.getY()) )
-                    && ( point.getX() < ( (this.points.get(j).getX() - this.points.get(i).getX()) * (point.getY() - this.points.get(i).getY()) / (this.points.get(j).getY() - this.points.get(i).getY()) + this.points.get(i).getX() ) ) )
-                res = !res;
-        }
-        return (res);
-    }
+	/**
+	 * Check if the polygon is "finite".
+	 * 
+	 * @return true if the Polygon contains at least 3 points.
+	 */
+	public boolean isFinite() {
+		return (this.points.size() > 2);
+	}
 }
