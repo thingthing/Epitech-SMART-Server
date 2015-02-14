57f81ce43fc33eeda5f9ac989b94142d4fc7bf8b
Vincent Buresi <vincent.buresi@gmail.com>
2015-02-14 09:37:16 +0100

    Correction of FileModelingManager

--- src/main/java/eip/smart/server/modeling/FileModelingManager.java
+++ src/main/java/eip/smart/server/modeling/FileModelingManager.java
@@ -1,136 +1,111 @@
 package eip.smart.server.modeling;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eip.smart.model.Modeling;
+import eip.smart.model.proxy.SimpleModelingProxy;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InvalidClassException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import eip.smart.model.Modeling;
-import eip.smart.model.proxy.SimpleModelingProxy;
 
 public class FileModelingManager extends ModelingManager {
 
-	private static String addExtension(String name) {
-		String res = name;
-		if (!res.matches(".*\\.modeling$"))
-			res += FileModelingManager.EXTENSION;
-		return (res);
-	}
+    private static File         DIR			= new File(new File(System.getProperty("catalina.base")).getAbsolutePath(), "modelings");
+    private static String		EXTENSION	= ".modeling";
+    private final static Logger LOGGER		= Logger.getLogger(Modeling.class.getName());
 
-	private final static Logger	LOGGER		= Logger.getLogger(Modeling.class.getName());
-	private static File			DIR			= new File(new File(System.getProperty("catalina.base")).getAbsolutePath(), "modelings");
+    public FileModelingManager() {
+        FileModelingManager.DIR.mkdirs();
+    }
 
-	private static String		EXTENSION	= ".modeling";
+    private static String addExtension(String name) {
+        String res = name;
+        if (!res.matches(".*\\.modeling$"))
+            res += FileModelingManager.EXTENSION;
+        return (res);
+    }
 
-	public FileModelingManager() {
-		FileModelingManager.DIR.mkdirs();
-	}
 
-	@Override
-	public boolean delete(String name) {
-		name = FileModelingManager.addExtension(name);
-		if (!this.exists(name))
-			return (false);
-		File file = new File(FileModelingManager.DIR, name);
-		file.delete();
-		return (true);
-	}
+    @Override
+    public boolean delete(String name) {
+        name = FileModelingManager.addExtension(name);
+        if (!this.exists(name))
+            return (false);
+        File file = new File(FileModelingManager.DIR, name);
+        file.delete();
+        return (true);
+    }
 
-	@Override
-	public boolean exists(String name) {
-		name = FileModelingManager.addExtension(name);
-		File file = new File(FileModelingManager.DIR, name);
-		return (file.exists());
-	}
+    @Override
+    public boolean exists(String name) {
+        name = FileModelingManager.addExtension(name);
+        File file = new File(FileModelingManager.DIR, name);
+        return (file.exists());
+    }
 
-	@Override
-	public ArrayList<SimpleModelingProxy> list() {
-		ArrayList<SimpleModelingProxy> modelings = new ArrayList<>();
-		for (File file : FileModelingManager.DIR.listFiles()) {
-			Modeling modeling = this.load(file.getName());
-			if (modeling != null)
-			{
-				SimpleModelingProxy smp = new SimpleModelingProxy(modeling);
-				modelings.add(smp);
-			}
-			else
-			{
-				SimpleModelingProxy smpObsolete = new SimpleModelingProxy();
-				smpObsolete.setName(file.getName());
-				smpObsolete.setObsolete(true);
-				modelings.add(smpObsolete);
-			}
-		}
-		return (modelings);
-	}
+    @Override
+    public ArrayList<SimpleModelingProxy> list() {
+        ArrayList<SimpleModelingProxy> modelings = new ArrayList<>();
+        for (File file : FileModelingManager.DIR.listFiles()) {
+            Modeling modeling = this.load(file.getName());
+            if (modeling != null)
+            {
+                SimpleModelingProxy smp = new SimpleModelingProxy(modeling);
+                modelings.add(smp);
+            }
+            else
+            {
+                SimpleModelingProxy smpObsolete = new SimpleModelingProxy();
+                smpObsolete.setName(file.getName());
+                smpObsolete.setObsolete(true);
+                modelings.add(smpObsolete);
+            }
+        }
+        return (modelings);
+    }
 
-	@Override
-	public Modeling load(String name) {
-		name = FileModelingManager.addExtension(name);
-		Modeling modeling = null;
-		if (!this.exists(name))
-			return (null);
-		File file = new File(FileModelingManager.DIR, name);
-		FileInputStream fis = null;
-		ObjectInputStream ois = null;
-		try {
-			fis = new FileInputStream(file);
-			ois = new ObjectInputStream(fis);
-			modeling = (Modeling) ois.readObject();
-			ois.close();
-			fis.close();
-		} catch (InvalidClassException e) {
-			FileModelingManager.LOGGER.log(Level.WARNING, "Saved modelisation (" + name + ") is obsolete and will be ignored.");
-			return (null);
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		try {
-			if (ois != null)
-				ois.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			if (fis != null)
-				fis.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return (modeling);
-	}
+    @Override
+    public Modeling load(String name) {
+        name = FileModelingManager.addExtension(name);
+        Modeling modeling = null;
+        if (!this.exists(name))
+            return (null);
+        File file = new File(FileModelingManager.DIR, name);
+        FileModelingManager.LOGGER.log(Level.INFO, "Loading modelisation at " + file.getAbsolutePath());
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            SimpleModelingProxy tmp = mapper.readValue(file, SimpleModelingProxy.class);
+            modeling = new Modeling();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return (modeling);
+    }
 
-	@Override
-	public void save(Modeling modeling) {
-		File file = new File(FileModelingManager.DIR, FileModelingManager.addExtension(modeling.getName()));
-		FileOutputStream fos = null;
-		ObjectOutputStream oos = null;
-		try {
-			fos = new FileOutputStream(file);
-			oos = new ObjectOutputStream(fos);
-			oos.writeObject(modeling);
-			oos.close();
-			fos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			if (oos != null)
-				oos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			if (fos != null)
-				fos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void save(Modeling modeling) {
+        File file = new File(FileModelingManager.DIR, FileModelingManager.addExtension(modeling.getName()));
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            SimpleModelingProxy tmp = new SimpleModelingProxy(modeling);
+            mapper.writeValue(file, tmp);
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        FileModelingManager.LOGGER.log(Level.INFO, "Saved modelisation at " + file.getAbsolutePath());
+    }
 }
