3b9ec75f7db3ef11b61049c588e1cecf0a4578bd
adrien rouchy <rouchy.adrien@gmail.com>
2015-04-11 18:37:25 +0200

    geometry test

--- src/test/java/eip/smart/server/PolygonTest.java
+++ src/test/java/eip/smart/server/PolygonTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import eip.smart.model.geometry.Point;
 import eip.smart.model.geometry.Polygon;
+import eip.smart.model.geometry.Rectangle;
+import eip.smart.model.geometry.Square;
 
 public class PolygonTest {
 	@BeforeClass
@@ -133,6 +135,26 @@ public class PolygonTest {
 		Assert.assertTrue(true);
 	}
 	
+	@Test
+	public void testPerimeterPentagone() {
+		int size = 5;
+		Polygon square = new Polygon();
+		
+		Point p1 = new Point(0, size, 0);
+		Point p2 = new Point(size, size, 0);
+		Point p3 = new Point(size, 0, 0);
+		Point p4 = new Point(0, 0, 0);
+		
+		square.add(p1);
+		square.add(p2);
+		square.add(p3);
+		square.add(p4);
+		
+		if (square.getPerimeter() != size * 4)
+			Assert.fail("square Perimeter error, result found is" + square.getPerimeter());
+		
+		Assert.assertTrue(true);
+	}
 	
 	@Test
 	public void testAreaTriangle() {
@@ -152,4 +174,145 @@ public class PolygonTest {
 		Assert.assertTrue(true);
 	}
 	
+	@Test
+	public void testTrueIncludesSquare() {
+		int size = 5;
+		Polygon square = new Polygon();
+
+		Point p1 = new Point(0, size, 0);
+		Point p2 = new Point(size, size, 0);
+		Point p3 = new Point(size, 0, 0);
+		Point p4 = new Point(0, 0, 0);
+
+		Point pointTest = new Point(0, size / 2, 0);
+		
+		square.add(p1);
+		square.add(p2);
+		square.add(p3);
+		square.add(p4);
+	
+		if (!square.includes(pointTest))
+			Assert.fail("square Includes error, result should be \"true\"");
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testFalseIncludesSquare() {
+		int size = 5;
+		Polygon square = new Polygon();
+
+		Point p1 = new Point(0, size, 0);
+		Point p2 = new Point(size, size, 0);
+		Point p3 = new Point(size, 0, 0);
+		Point p4 = new Point(0, 0, 0);
+
+		Point pointTest = new Point(0, size + 1, 0);
+		
+		square.add(p1);
+		square.add(p2);
+		square.add(p3);
+		square.add(p4);
+	
+		if (square.includes(pointTest))
+			Assert.fail("square Includes error, result should be \"false\"");
+		
+		Assert.assertTrue(true);
+	}
+
+	
+	@Test
+	public void testTrueIncludesTriangle() {
+		int size = 5;
+		Polygon triangle = new Polygon();
+		Point p1 = new Point(0, 5, 0);
+		Point p2 = new Point(5, 0, 0);
+		Point p3 = new Point(0, 0, 0);
+		
+		Point pointTest = new Point(0, size / 2, 0);
+
+		triangle.add(p1);
+		triangle.add(p2);
+		triangle.add(p3);
+		
+		if (!triangle.includes(pointTest))
+			Assert.fail("triangle Includes error, result should be \"true\"");		
+		Assert.assertTrue(true);
+	}
+	
+	
+	@Test
+	public void testfalseIncludesTriangle() {
+		int size = 5;
+		Polygon triangle = new Polygon();
+		Point p1 = new Point(0, 5, 0);
+		Point p2 = new Point(5, 0, 0);
+		Point p3 = new Point(0, 0, 0);
+		
+		Point pointTest = new Point(0, size + 2, 0);
+
+		triangle.add(p1);
+		triangle.add(p2);
+		triangle.add(p3);
+		
+		if (triangle.includes(pointTest))
+			Assert.fail("triangle Includes error, result should be \"false\"");
+		
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testSquare()
+	{
+		int size = 5;
+		Polygon square = new Polygon();
+		
+		Point p1 = new Point(0, size, 0);
+		Point p2 = new Point(size, size, 0);
+		Point p3 = new Point(size, 0, 0);
+		Point p4 = new Point(0, 0, 0);
+		
+		square.add(p1);
+		square.add(p2);
+		square.add(p3);
+		square.add(p4);
+		
+		Polygon square2 = new Square(new Point(0, 0, 0), size);
+		if (square2.getPoints().size() != 4)
+			Assert.fail("error, wrong faces number");
+		else if (square2.getPerimeter() != square.getPerimeter())
+			Assert.fail("error; wrong perimeter");
+		else if (square2.getArea() != square.getArea())
+			Assert.fail("error; wrong area");	
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testRectangle()
+	{
+		int size1 = 5;
+		int size2 = 10;
+		
+		Polygon rectangle = new Polygon();
+		
+		Point p1 = new Point(0, size1, 0);
+		Point p2 = new Point(size2, size1, 0);
+		Point p3 = new Point(size2, 0, 0);
+		Point p4 = new Point(0, 0, 0);
+		
+		rectangle.add(p1);
+		rectangle.add(p2);
+		rectangle.add(p3);
+		rectangle.add(p4);
+		
+		Polygon rectangle2 = new Rectangle(new Point(0, 0, 0), size1, size2);
+		if (rectangle2.getPoints().size() != 4)
+			Assert.fail("error, wrong faces number");
+		else if (rectangle2.getPerimeter() != rectangle.getPerimeter())
+			Assert.fail("error; wrong perimeter");
+		else if (rectangle2.getArea() != rectangle.getArea())
+			Assert.fail("error; wrong area");
+		Assert.assertTrue(true);
+	}
+
 }
