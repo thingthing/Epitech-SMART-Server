e0f68e9ad62f4f29393f5310bfbb09318f420f39
Pierre Demessence <pierredemessence@gmail.com>
2015-05-07 00:58:38 +0200

    keyExists() now also checks in the default configuration.

--- src/main/java/eip/smart/server/util/Configuration.java
+++ src/main/java/eip/smart/server/util/Configuration.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -38,8 +39,26 @@ public class Configuration {
 	 *            The name of the configuration to check.
 	 * @return true if the configuration file exists.
 	 */
-	public static boolean exists(String name) {
+	public static boolean confExists(String name) {
 		return (Configuration.defaultProperties.containsKey(name) || new File(Configuration.CONFIG_DIR, name + Configuration.CONFIG_EXTENSION).exists());
+	}
+
+	/**
+	 * Get a list of all the configuration names.
+	 *
+	 * @return
+	 */
+	public static ArrayList<String> getConfigurations() {
+		ArrayList<String> names = new ArrayList<>();
+		for (String name : new File(Configuration.CONFIG_DIR).list())
+			names.add(name.replaceAll(Configuration.CONFIG_EXTENSION, ""));
+		return (names);
+	}
+
+	private static Properties getDefaultProperties(String name) {
+		if (Configuration.defaultProperties.get(name) == null)
+			Configuration.defaultProperties.put(name, new Properties());
+		return (Configuration.defaultProperties.get(name));
 	}
 
 	/**
@@ -50,9 +69,7 @@ public class Configuration {
 	 * @param value
 	 */
 	public static void setDefaultProperty(String name, String key, String value) {
-		if (Configuration.defaultProperties.get(name) == null)
-			Configuration.defaultProperties.put(name, new Properties());
-		Configuration.defaultProperties.get(name).setProperty(key, value);
+		Configuration.getDefaultProperties(name).setProperty(key, value);
 	}
 
 	private File		configFile;
@@ -70,9 +87,7 @@ public class Configuration {
 	 */
 	public Configuration(String name) {
 		this.name = name;
-		if (Configuration.defaultProperties.get(name) == null)
-			Configuration.defaultProperties.put(name, new Properties());
-		this.properties = new Properties(Configuration.defaultProperties.get(name));
+		this.properties = new Properties(Configuration.getDefaultProperties(name));
 		this.configFile = new File(Configuration.CONFIG_DIR, name + Configuration.CONFIG_EXTENSION);
 		new File(Configuration.CONFIG_DIR).mkdirs();
 		if (this.configFile.exists()) {
@@ -92,15 +107,6 @@ public class Configuration {
 		}
 
 		this.save();
-	}
-
-	/**
-	 * @param key
-	 * @return
-	 * @see java.util.Hashtable#containsKey(java.lang.Object)
-	 */
-	public boolean exists(Object key) {
-		return (this.properties.containsKey(key));
 	}
 
 	/**
@@ -130,13 +136,22 @@ public class Configuration {
 	}
 
 	/**
+	 * @param key
+	 * @return
+	 * @see java.util.Hashtable#containsKey(java.lang.Object)
+	 */
+	public boolean keyExists(Object key) {
+		return (Configuration.getDefaultProperties(this.name).containsKey(key) || this.properties.containsKey(key));
+	}
+
+	/**
 	 * Print the current configuration to standard output.
 	 */
 	public void printDebug() {
 		System.out.println("#ConfigurationDebugBegin#");
 		if (Configuration.defaultProperties.get(this.name) != null)
 			for (Entry<Object, Object> entry : Configuration.defaultProperties.get(this.name).entrySet())
-				if (!this.exists(entry.getKey()))
+				if (!this.keyExists(entry.getKey()))
 					System.out.format("[%s] : %s (default value)\n", entry.getKey(), entry.getValue());
 		for (Entry<Object, Object> entry : this.properties.entrySet())
 			System.out.format("[%s] : %s\n", entry.getKey(), entry.getValue());
