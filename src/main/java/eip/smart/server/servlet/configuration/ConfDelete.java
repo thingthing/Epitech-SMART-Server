5f137ee596a77c6f8b09e2e19e7bf0d62dda3b4a
Pierre Demessence <pierredemessence@gmail.com>
2015-07-02 15:27:22 +0200

    Fix wrong Status value.

--- src/main/java/eip/smart/server/servlet/configuration/ConfDelete.java
+++ src/main/java/eip/smart/server/servlet/configuration/ConfDelete.java
@@ -1,16 +1,18 @@
 package eip.smart.server.servlet.configuration;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import eip.smart.model.Status;
-import eip.smart.server.exception.StatusException;
-import eip.smart.server.servlet.JsonServlet;
-import eip.smart.server.util.Configuration;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import eip.smart.model.Status;
+import eip.smart.server.exception.StatusException;
+import eip.smart.server.servlet.JsonServlet;
+import eip.smart.server.util.Configuration;
 
 /**
  * @author Vincent Buresi
@@ -18,11 +20,11 @@ import java.io.IOException;
 @WebServlet("/conf_delete")
 public class ConfDelete extends JsonServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp, JsonGenerator json) throws ServletException, IOException, StatusException {
-        String name = JsonServlet.getParameter(req, "name");
-        if (! Configuration.confExists(name))
-            throw new StatusException(Status.DOES_NOT_EXIST.addObjects("configuration", "name", name));
-        Configuration.confDelete(name);
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp, JsonGenerator json) throws ServletException, IOException, StatusException {
+		String name = JsonServlet.getParameter(req, "name");
+		if (!Configuration.confExists(name))
+			throw new StatusException(Status.NOT_FOUND.addObjects("configuration", "name", name));
+		Configuration.confDelete(name);
+	}
 }
