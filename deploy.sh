78455e77c21752714552c2b852fbbb9b7117d82f
Pierre Demessence <pierre.demessence@gmail.com>
2015-08-19 20:15:42 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #Server Deployement Script
 mvn clean install
-/usr/share/apache-tomcat-7.0.57/bin/catalina.sh stop -force
+sudo /usr/share/apache-tomcat-7.0.57/bin/catalina.sh stop -force
 rm -rf /usr/share/apache-tomcat-7.0.57/webapps/smartserver/
 sudo /usr/share/apache-tomcat-7.0.57/bin/catalina.sh start
 mvn tomcat7:redeploy
