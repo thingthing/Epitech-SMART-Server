f60f57cbefaa4e6705e3996b2326dc549ccbab73
Pierre Demessence <pierre.demessence@gmail.com>
2015-08-30 10:38:04 +0000

    Will now discard packet that don't have data or status object in the json after parsing. TCPPacketDecoder.java edited online with Bitbucket

--- src/main/java/eip/smart/server/net/tcp/TCPPacketDecoder.java
+++ src/main/java/eip/smart/server/net/tcp/TCPPacketDecoder.java
@@ -20,7 +20,6 @@ public class TCPPacketDecoder extends ProtocolDecoderAdapter {
 	@Override
 	public void decode(IoSession session, IoBuffer in, ProtocolDecoderOutput out) throws Exception {
 		try {
-			TCPPacketDecoder.LOGGER.debug("Endianness : {}", ByteOrder.nativeOrder());
 			TCPPacketDecoder.LOGGER.debug("Received TCP packet of size {} from {}", in.remaining(), session.getRemoteAddress());
 			TCPPacketDecoder.LOGGER.debug("HexDump : {}", in.getHexDump());
 			if (in.remaining() >= TCPPacket.HEADER_SIZE) {
@@ -80,6 +79,18 @@ public class TCPPacketDecoder extends ProtocolDecoderAdapter {
 				} catch (JsonMappingException e) {
 					TCPPacketDecoder.LOGGER.warn("TCP packet discarded :", e);
 					return;
+				}
+				if (jsonPayload.get("data") == null) {
+					TCPPacketDecoder.LOGGER.warn("TCP packet discarded : no \"data\" json object after parsing");
+					return ;
+				}
+				if (jsonPayload.get("status") == null) {
+					TCPPacketDecoder.LOGGER.warn("TCP packet discarded : no \"status\" json object after parsing");
+					return ;
+				}
+				if (jsonPayload.get("status").get("code") == null) {
+					TCPPacketDecoder.LOGGER.warn("TCP packet discarded : no \"code\" json object in \"status\" after parsing");
+					return ;
 				}
 
 				// Good packet
