098e92103c657ae58742af2490c4c2d5a76abbd2
Pierre Demessence <pierre.demessence@gmail.com>
2015-08-30 15:19:16 +0000

    Fixed log clean LogCleanServlet.java edited online with Bitbucket

--- src/main/java/eip/smart/server/servlet/LogCleanServlet.java
+++ src/main/java/eip/smart/server/servlet/LogCleanServlet.java
@@ -1,6 +1,8 @@
 package eip.smart.server.servlet;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.SecurityException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,8 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import eip.smart.model.Status;
+
 @WebServlet("/log_clean")
-public class LogCleanServlet extends HttpServlet {
+public class LogCleanServlet extends JsonServlet {
 
     private final static Logger	LOGGER	= LoggerFactory.getLogger(LogCleanServlet.class);
 
@@ -20,7 +24,12 @@ public class LogCleanServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
 		String filename = System.getProperty("catalina.base") + "/log/log.html";
 		File log = new File(filename);
-        log.delete();
+		try {
+        	log.delete();
+			log.createNewFile();
+		} catch (IOException | SecurityException e) {
+			throw new StatusException(Status.ERR_UNKNOWN.addObjects(e.getMessage()));
+		}
         LogCleanServlet.LOGGER.info("Log cleaned.");
 	}
 }
