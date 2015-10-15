5df7ca5f51453f8933c89718a24078f7167f5c8b
Pierre Demessence <pierredemessence@gmail.com>
2015-10-15 16:42:17 +0200

    Cleaned Slack Appender

--- src/main/java/eip/smart/server/logging/SlackAppender.java
+++ src/main/java/eip/smart/server/logging/SlackAppender.java
@@ -24,75 +24,76 @@ import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 
 public class SlackAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
 
+	@SuppressWarnings({ "hiding" })
+	class SlackAttachment {
+
+		class Field implements JsonSerializable {
+			public String	title;
+			public String	value;
+
+			public Field(String title, String value) {
+				this.title = title;
+				this.value = value;
+			}
+
+			@Override
+			public void serialize(JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+				jgen.writeStartObject();
+				jgen.writeObjectField("title", this.title);
+				jgen.writeObjectField("value", this.value);
+				jgen.writeObjectField("short", true);
+				jgen.writeEndObject();
+			}
+
+			@Override
+			public void serializeWithType(JsonGenerator jgen, SerializerProvider provider, TypeSerializer typeSer) throws IOException, JsonProcessingException {}
+		}
+
+		public String			author_icon	= "http://blogs.thesitedoctor.co.uk/tim/images/616c06524d21_ABF1/error2.png";
+		public String			author_link	= "http://54.148.17.11:8080";
+		public String			author_name;
+		public String			color		= "#FF0000";
+		public String			fallback;
+		public ArrayList<Field>	fields		= new ArrayList<>();
+		public String[]			mrkdwn_in	= { "text" };
+		public String			text;
+		public String			title;
+		public String			title_link	= "http://54.148.17.11:8080";
+
+		public SlackAttachment(final ILoggingEvent evt) {
+			this.fields.add(new Field("Class", evt.getCallerData()[0].getClassName()));
+			this.fields.add(new Field("Method", evt.getCallerData()[0].getMethodName() + ":" + evt.getCallerData()[0].getLineNumber()));
+			this.fields.add(new Field("Date", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS").format(new Date(evt.getTimeStamp()))));
+			this.fields.add(new Field("Thread", evt.getThreadName()));
+			this.author_name = "Critical Server Error";
+			this.fallback = this.author_name + "\n" + evt.getFormattedMessage();
+			this.text = "*" + evt.getFormattedMessage() + "*\n";
+			if (evt.getThrowableProxy() != null)
+				this.text += "```" + this.getExceptionStackTrace(evt.getThrowableProxy()) + "```";
+		}
+
+		public String getExceptionStackTrace(IThrowableProxy ex) {
+			String str = ex.getClassName() + ": " + ex.getMessage() + "\n";
+			for (int i = 0; i < ex.getStackTraceElementProxyArray().length - ex.getCommonFrames(); i++) {
+				StackTraceElementProxy trace = ex.getStackTraceElementProxyArray()[i];
+				StackTraceElement ste = trace.getStackTraceElement();
+				str += "\tat " + ste.getClassName() + "." + ste.getMethodName() + "(" + ste.getFileName() + ":" + ste.getLineNumber() + ") [" + trace.getClassPackagingData().getCodeLocation() + ":" + trace.getClassPackagingData().getVersion() + "]\n";
+			}
+			if (ex.getCommonFrames() != 0)
+				str += "\t... " + ex.getCommonFrames() + " common frames omitted\n";
+			if (ex.getCause() != null)
+				str += "Caused by:" + this.getExceptionStackTrace(ex.getCause());
+			return (str);
+		}
+	}
+
 	private final static String	API_URL	= "https://slack.com/api/chat.postMessage";
-	private String				token;
 	private String				channel;
+	private String				token;
 	private String				username;
 
 	@Override
 	protected void append(final ILoggingEvent evt) {
-		@SuppressWarnings({ "unused", "hiding" })
-		class SlackAttachment {
-
-			class Field implements JsonSerializable {
-				public String	title;
-				public String	value;
-
-				public Field(String title, String value) {
-					this.title = title;
-					this.value = value;
-				}
-
-				@Override
-				public void serialize(JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
-					jgen.writeStartObject();
-					jgen.writeObjectField("title", this.title);
-					jgen.writeObjectField("value", this.value);
-					jgen.writeObjectField("short", true);
-					jgen.writeEndObject();
-				}
-
-				@Override
-				public void serializeWithType(JsonGenerator jgen, SerializerProvider provider, TypeSerializer typeSer) throws IOException, JsonProcessingException {}
-			}
-
-			public String			title;
-			public String			title_link	= "http://54.148.17.11:8080";
-			public String			fallback;
-			public String			color		= "#FF0000";
-			public String			text;
-			public ArrayList<Field>	fields		= new ArrayList<>();
-			public String[]			mrkdwn_in	= { "text" };
-			public String			author_name;
-			public String			author_link	= "http://54.148.17.11:8080";
-			public String			author_icon	= "http://blogs.thesitedoctor.co.uk/tim/images/616c06524d21_ABF1/error2.png";
-
-			public SlackAttachment(final ILoggingEvent evt) {
-				this.fields.add(new Field("Class", evt.getCallerData()[0].getClassName()));
-				this.fields.add(new Field("Method", evt.getCallerData()[0].getMethodName() + ":" + evt.getCallerData()[0].getLineNumber()));
-				this.fields.add(new Field("Date", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss,SSS").format(new Date(evt.getTimeStamp()))));
-				this.fields.add(new Field("Thread", evt.getThreadName()));
-				this.author_name = "Critical Server Error";
-				this.fallback = this.author_name + "\n" + evt.getFormattedMessage();
-				this.text = "*" + evt.getFormattedMessage() + "*\n";
-				if (evt.getThrowableProxy() != null)
-					this.text += "```" + this.getExceptionStackTrace(evt.getThrowableProxy()) + "```";
-			}
-
-			public String getExceptionStackTrace(IThrowableProxy ex) {
-				String str = ex.getClassName() + ": " + ex.getMessage() + "\n";
-				for (int i = 0; i < ex.getStackTraceElementProxyArray().length - ex.getCommonFrames(); i++) {
-					StackTraceElementProxy trace = ex.getStackTraceElementProxyArray()[i];
-					StackTraceElement ste = trace.getStackTraceElement();
-					str += "\tat " + ste.getClassName() + "." + ste.getMethodName() + "(" + ste.getFileName() + ":" + ste.getLineNumber() + ") [" + trace.getClassPackagingData().getCodeLocation() + ":" + trace.getClassPackagingData().getVersion() + "]\n";
-				}
-				if (ex.getCommonFrames() != 0)
-					str += "\t... " + ex.getCommonFrames() + " common frames omitted\n";
-				if (ex.getCause() != null)
-					str += "Caused by:" + this.getExceptionStackTrace(ex.getCause());
-				return (str);
-			}
-		}
 		try {
 			final URL url = new URL(SlackAppender.API_URL);
 
