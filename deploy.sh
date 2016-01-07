74016a1ae980a003dc9f0e371d7bd7f3b5e7502f
Pierre Demessence <pierredemessence@gmail.com>
2016-01-07 15:50:59 +0100

    lolwut

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #Server Deployement Script
 mvn clean install
 sudo /usr/share/apache-tomcat-7.0.57/bin/catalina.sh stop -force
-sudo rm -rf /usr/share/apache-tomcat-7.0.57/webapps/smartserver/
+sudo rm -rf /usr/share/apache-tomcat-7.0.57/webapps/ROOT/
 sudo /usr/share/apache-tomcat-7.0.57/bin/catalina.sh start
 mvn tomcat7:redeploy clean
 #mvn site:run
