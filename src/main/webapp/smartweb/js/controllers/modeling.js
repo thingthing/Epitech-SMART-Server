51ab27131c89972f0de2a639c6153cfa482cc566
Pierre Demessence <pierredemessence@gmail.com>
2016-01-14 11:41:03 +0100

    Added FirstPersonController for camera. Modified way points are shown.

--- src/main/webapp/smartweb/js/controllers/modeling.js
+++ src/main/webapp/smartweb/js/controllers/modeling.js
@@ -31,26 +31,48 @@ angular.module('SMARTApp.controllers')
 	var camera, scene, renderer, particles, geometry, material, i, color, sprite, size, controls;
 	
 	camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 20000 );
-	camera.position.z = 1000;
+	//camera.position.z = 1000;
+	window.camera = camera
 
 	scene = new THREE.Scene();
 
-	controls = new THREE.TrackballControls( camera, canvas );
-	controls.target.set( 0, 0, 0 );
-	
-	controls.rotateSpeed = 4.0;
-	controls.zoomSpeed = 1.2;
-	controls.panSpeed = 1.0;
-	
-	controls.staticMoving = false;
+	if (true) {
+		controls = new THREE.FirstPersonControls(camera, canvas);
+
+		controls.movementSpeed = 1;
+		controls.lookSpeed = 0.15;
+		controls.noFly = false;
+		controls.lookVertical = true;
+	} else {
+
+		controls = new THREE.TrackballControls(camera, canvas);
+
+		controls.rotateSpeed = 4.0;
+		controls.zoomSpeed = 1.2;
+		controls.panSpeed = 1.0;
+
+		controls.staticMoving = false;
+		controls.dynamicDampingFactor = 0.25
+	}
+
+	controls.target.set(0, 0, 0);
+	window.controls = controls;
 
 	renderer = new THREE.WebGLRenderer({ canvas: canvas });
 	renderer.setPixelRatio( window.devicePixelRatio );
 
+	var clock = new THREE.Clock();
+	$scope.render = function() {
+		renderer.render( scene, camera );
+	}
+
 	var req;
-	function animate() {
-		req = requestAnimationFrame( animate );
-		controls.update()
+	$scope.animate = function() {
+		req = requestAnimationFrame( $scope.animate );
+		var delta = clock.getDelta(),
+			time = clock.getElapsedTime() * 5;
+		//console.log(delta);
+		controls.update(delta)
 		if (THREEx.FullScreen.activated())
 			renderer.setSize( width, height );
 		else if (renderer.getSize().width != 800)
@@ -58,9 +80,11 @@ angular.module('SMARTApp.controllers')
 		$scope.render();
 	}
 
-	$scope.render = function() {
-		renderer.render( scene, camera );
-	}
+	var helper = new THREE.GridHelper( 500, 10 );
+	helper.color1.setHex( 0x444444 );
+	helper.color2.setHex( 0x444444 );
+	helper.position.y = 0.1;
+	scene.add( helper );
 
     $http.get($scope.server + '/get_points').success(function(data) {
 		geometry = new THREE.Geometry();
@@ -69,19 +93,23 @@ angular.module('SMARTApp.controllers')
     		var vertex = new THREE.Vector3();
 			var point = data.data.pointcloud.points[i];
     		vertex.x = point.x;
-    		vertex.y = point.y;
-    		vertex.z = point.z;
-			colors[i] = new THREE.Color("rgb("+(point.color.red)+","+(point.color.green+20)+","+(point.color.blue)+")");
-    		geometry.vertices.push( vertex );
+    		vertex.y = -point.y;
+    		vertex.z = -point.z;
+			if (vertex.x && vertex.y && vertex.z) {
+				colors.push(new THREE.Color("rgb(" + (point.color.red) + "," + (point.color.green + 20) + "," + (point.color.blue) + ")"));
+				geometry.vertices.push(vertex);
+			}
     	}
+		controls.target.set( data.data.pointcloud.points[i].x, -data.data.pointcloud.points[i].y, -data.data.pointcloud.points[i].z );
     	geometry.colors = colors;
     	sprite = THREE.ImageUtils.loadTexture( "css/img/ball.png" );
-    	material = new THREE.PointsMaterial( { size: (6 - 2) / 1000, sizeAttenuation: true, vertexColors: THREE.VertexColors, map: sprite, alphaTest: 0.1, transparent: true } );
+    	material = new THREE.PointsMaterial( { size: (25 - 2) / 1000, sizeAttenuation: true, vertexColors: THREE.VertexColors, map: sprite, alphaTest: 0.1, transparent: false } );
+		window.material = material
 
     	particles = new THREE.Points( geometry, material );
     	
     	scene.add( particles );
-    	animate();
+    	$scope.animate();
     });
 	window.renderer = renderer;
 
@@ -94,6 +122,7 @@ angular.module('SMARTApp.controllers')
 		camera.aspect = width / height;
 		camera.updateProjectionMatrix();
 		renderer.setSize( width, height );
+		controls.handleResize();
 	}
 
     startIntervals();
